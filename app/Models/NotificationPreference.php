8336e2ebcf465185e542b40e5be41312e2b2ec11
Akash Kumar <akashkr15082000@gmail.com>
2026-06-25 15:13:36 +0530

    fix: add fillable fields to NotificationPreference model

--- app/Models/NotificationPreference.php
+++ app/Models/NotificationPreference.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotificationPreference extends Model
 {
-    //
+    public $incrementing = false;
+    protected $keyType   = 'string';
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'push_enabled',
+        'email_enabled',
+        'sms_enabled',
+        'meeting_alerts',
+        'sos_alerts',
+        'chat_notifications',
+        'marketing_emails',
+        'created_at',
+        'updated_at',
+    ];
 }
